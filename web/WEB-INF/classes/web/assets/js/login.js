6bb8dd4f333d952a437f3f579034590166f20ec4
chen xiaohua <myischenxiaohua@163.com>
2019-01-14 21:56:22 +0800

    验证码单击获取、登录验证

---
+++ web/WEB-INF/classes/web/assets/js/login.js
@@ -0,0 +1,30 @@
+$(function () {
+    //点击图片获取验证码
+    $("input[id=\"code\"]").next().click(function () {
+        $(this).attr('src','/book/IdentityServlet/IdentifyingCode?tm='+new Date());
+
+    })
+
+    //点击按钮登录
+   $("button[type=\"button\"]").click(function () {
+       var name=$("#username").val();
+       var pwd=$("#password").val();
+       var code=$("#code").val();
+       var url='http://localhost:8080/book/admin/AdminServlet/login';
+       $.ajaxAction(url,"post",{user:name,password:pwd,code:code},[cheackLogin]);
+
+   })
+
+
+
+})
+
+function cheackLogin(data,parm) {
+    if(data.status){
+        location.href=data.url
+    }else {
+       alert(data.msg)
+    }
+    console.log(data)
+
+}
